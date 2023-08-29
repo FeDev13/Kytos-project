a30bc587a735176d8480ef4d6c39fb07ade4c61c
Federico Robledo <federicorobledo85@gmail.com>
2023-08-29 14:00:52 -0300

    first commit

---
+++ client/medical-patients/src/components/Sidebar.jsx
@@ -0,0 +1,65 @@
+import { NavLink } from "react-router-dom";
+import logo from "../assets/descarga.jpg";
+import {
+  BsFillPersonLinesFill,
+  BsFillPersonBadgeFill,
+  BsFillPersonPlusFill,
+} from "react-icons/bs";
+
+export const Sidebar = () => {
+  return (
+    <div className="bg-primary min-w-max min-h-screen h-auto text-secondary font-PTSans">
+      <div className="mt-11 w-full px-8 font-PTSans text-center">
+        <img src={logo} alt="logo CIS kinesiologia" className=" rounded-lg" />
+        <h1 className="tracking-wide text-2xl my-9 font-bold">Menu</h1>
+        <hr />
+        <ul className="text-start text-xl">
+          <NavLink
+            to="/home"
+            className={({ isActive }) =>
+              isActive ? "text-tertiary" : "text-secondary"
+            }
+          >
+            <li className="my-5 flex justify-start items-center w-full hover:text-tertiary hover: transition-colors">
+              {" "}
+              <span className="flex items-center">
+                <BsFillPersonLinesFill className="w-8 h-w-8" />
+              </span>
+              <p className="pl-2">Pacientes</p>
+            </li>
+          </NavLink>
+          <hr />
+          <NavLink
+            to="/urgentPatients"
+            className={({ isActive }) =>
+              isActive ? "text-tertiary" : "text-secondary"
+            }
+          >
+            <li className="my-5 flex justify-start items-center hover:text-tertiary hover: transition-colors">
+              {" "}
+              <span className="flex items-center">
+                <BsFillPersonBadgeFill className="w-8 h-w-8" />
+              </span>
+              <p className="pl-2">Pacientes urgentes</p>
+            </li>
+          </NavLink>
+          <hr />
+          <NavLink
+            to="/registerNewPatient"
+            className={({ isActive }) =>
+              isActive ? "text-tertiary" : "text-secondary"
+            }
+          >
+            <li className="my-5 flex justify-start items-center hover:text-tertiary hover: transition-colors">
+              {" "}
+              <span className="flex items-center">
+                <BsFillPersonPlusFill className="w-8 h-w-8" />
+              </span>
+              <p className="pl-2">Agregar paciente</p>
+            </li>
+          </NavLink>
+        </ul>
+      </div>
+    </div>
+  );
+};
